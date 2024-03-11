032807c9e064f1aa265b016cc3201bff1a78a9f3
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-03-11 16:58:01 +0200

    Task1-Task5 running test

---
+++ src/main/java/lab2/task4/OtherRunner.java
@@ -0,0 +1,33 @@
+package lab2.task4;
+
+public class OtherRunner {
+    public static void main(String[] args) {
+        (new PB(2)).print();
+    }
+}
+
+class PA {
+    protected int value;
+
+    public PA(int value) {
+        add(value);
+    }
+
+    private void add(int value) {
+        this.value += value;
+    }
+
+    public void print() {
+        System.out.println(value);
+    }
+}
+
+class PB extends PA {
+    public PB(int value) {
+        super(value);
+    }
+
+    private void add(int value) {
+        this.value += value * 2;
+    }
+}
